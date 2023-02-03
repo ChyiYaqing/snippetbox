af58e7c71cc333a80f25f36c082d471d08ae186d
chyiyaqing <chyiyaqing@gamil.com>
2023-02-03 21:22:26 +0800

    chapter 2.4 finish

--- main.go
+++ main.go
@@ -24,6 +24,24 @@ func sinppetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not.
+	if r.Method != "POST" {
+		// Use the Header().Set() method to add an 'Allow: POST' header to the
+		// response header map. The first parameter is the header name, and
+		// the second parameter is the header value.
+		w.Header().Set("Allow", http.MethodPost)
+
+		// If it's not, use the w.WriteHeader() method to send a 405 status
+		// code and the w.Write() method to write a "Method Not Allowed"
+		// response body. We then return from the function so that the
+		// subsequent code is not executed.
+		w.WriteHeader(http.StatusMethodNotAllowed) // once per response
+		// If don't call w.WriteHeader() explicitly, then the first call to w.Write()
+		// will aytomatically send a 200 OK status code to the user.
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
@@ -32,7 +50,6 @@ func main() {
 	// Use the http.NewServeMux() function to initialize a new servemux, then
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
-
 	// URL patterns: subtree paths
 	mux.HandleFunc("/", home)
 
