b6f0a7ddb75a15b7837de26fd06995f9f71cb13a
chyiyaqing <chyiyaqing@gamil.com>
2023-02-04 21:26:14 +0800

    chapter 3.1 finish

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
 func main() {
+	// addr := flag.String("addr", ":4000", "HTTP network address")
+	// flag.Parse()
+	//addr := os.Getenv("SNIPPETBOX_ADDR")
+
+	var cfg config
+
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
@@ -16,7 +32,7 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Staring server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Staring server on %s", cfg.addr)
+	err := http.ListenAndServe(cfg.addr, mux)
 	log.Fatal(err)
 }
